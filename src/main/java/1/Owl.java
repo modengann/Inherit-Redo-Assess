4b8c0e645f9c20dd510b73b35e8c81dc93934d2d
modengann <99417989+modengann@users.noreply.github.com>
2022-05-31 13:12:16 -0400

    first commit

---
+++ src/main/java/1/Owl.java
@@ -0,0 +1,21 @@
+
+
+public class Owl {
+    private String name;
+
+	public Owl(){
+		name = "";
+	}
+
+	public Owl(String owlName){
+		name = owlName;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public String getFood(){
+		return "furry animals, insects, or small birds";
+	}
+}
